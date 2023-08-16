c5da69334d7db6e9e890600c5ab1d2dfebb89304
e_rezovsky <e.rezovsky@gmail.com>
2023-08-16 21:37:10 +0500

    edit vodel

--- reader/models.py
+++ reader/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Reader(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    clas = models.PositiveIntegerField()
+    class_letter = models.CharField(max_length=1)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.clas}{self.class_letter}"
