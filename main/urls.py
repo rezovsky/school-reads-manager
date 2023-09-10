c877b2fac7fc336e8a991a54a6437e94fb74df8b
rezovsky <e.rezovsky@gmail.com>
2023-09-10 17:53:48 +0500

    edit base.html, add main.html

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('main', views.main, name='main'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'main/index.html')
+
+
+def main(request):
+    return render(request, 'main/main.html')
