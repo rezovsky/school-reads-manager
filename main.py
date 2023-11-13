fdeb4bdaa0fdc6070f9a5a502904c878caaa8092
rezovskiy_em <e.rezovsky@gmail.com>
2023-11-14 03:18:27 +0500

    add index.html

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    with open("public/index.html") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
 
 
 @app.get("/test/{test}")
