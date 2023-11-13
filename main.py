137a1635b10e22f3c6b2d27dd06a313d0bdbe9bf
rezovskiy_em <e.rezovsky@gmail.com>
2023-11-14 03:44:45 +0500

    edit index.html

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from starlette.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
