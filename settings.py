0084545693d2cae166319a98087499934685fe68
nout <e.rezovsky@gmail.com>
2023-11-16 14:05:51 +0500

    edit settings.py

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ env = Env()
 
 REAL_DATABASE_URL = env.str(
     "REAL_DATABASE_URL",
-    default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/postgres"
+    default="postgresql+asyncpg://postgres:postgres@10.0.0.13:5432/postgres"
 )
