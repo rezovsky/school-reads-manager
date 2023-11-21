40a200e4a30fa5a474e6c263b114866293d789af
rezovsky <e.rezovsky@gmail.com>
2023-11-21 14:49:11 +0500

    edit main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+import re
+import uuid
+
+import uvicorn
+from fastapi import FastAPI, HTTPException, APIRouter
+from pydantic import BaseModel, EmailStr, validator
+from sqlalchemy import Column, String, Boolean
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import declarative_base
-from starlette.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import settings
 
@@ -16,20 +22,111 @@ async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncS
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String(50), nullable=False)
+    surname = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    is_active = Column(Boolean, default=True)
 
 
-app = FastAPI()
+# LOGIC
+
+class UsrDAL:
+    def __int__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def create_user(
+            self, name: str, surname: str, email: str
+    ) -> User:
+        new_user = User(
+            name=name,
+            surname=surname,
+            email=email,
+        )
+        self.db_session.add(new_user)
+        await self.db_session.flush()
+        return new_user
+
+
+# MODELS API
+
+LETTER_MATCH_PATTERN = re.compile(r"^[a-zA-Zа-яА-Я\-]+$")
+
+
+class TunedModel(BaseModel):
+    class config:
+        orm_mode = True
+
+
+class ShowUser(TunedModel):
+    user_id: uuid.UUID
+    name: str
+    surname: str
+    email: EmailStr
+    is_active: bool
+
+
+class UserCreate(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+
+    @validator("name")
+    def validate_name(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Name must contain only letters"
+            )
+        return value
+
+    @validator("surname")
+    def validate_surname(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Surname must contain only letters"
+            )
+        return value
+
+
+# API ROUTERS
+
+app = FastAPI(title="school-reads-manager")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
-@app.get("/")
-async def root():
-    with open("public/index.html") as file:
-        html_content = file.read()
-    return HTMLResponse(content=html_content, status_code=200)
+user_router = APIRouter()
 
 
-@app.get("/test/{test}")
-async def testing(test: int):
-    return {"test": test}
+async def _create_new_user(body: UserCreate) -> ShowUser:
+    async with async_session() as session:
+        async with session.begin():
+            user_dal = UsrDAL(session)
+            user = await user_dal.create_user(
+                name=body.name,
+                surname=body.surname,
+                email=body.email,
+            )
+            return ShowUser(
+                user_id=user.user_id,
+                name=user.name,
+                surname=user.surname,
+                email=user.email,
+                is_active=user.is_active,
+            )
+
+
+@user_router.post("/", response_model=ShowUser)
+async def create_user(body: UserCreate) -> ShowUser:
+    return await _create_new_user(body)
+
+
+main_api_router = APIRouter()
+
+main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(main_api_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
