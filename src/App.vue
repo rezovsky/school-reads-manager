083a69b5435f5d03e8e922a40fa1c2131a33bc0a
rezovsky <e.rezovsky@gmail.com>
2023-11-30 03:26:23 +0500

    edit vue script

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>Hello</div>
+  <TextbookList v-bind:textbooks="textbooks"/>
 </template>
 
 <script>
-
+import TextbookList from "@/components/TextbookList.vue";
+export default {
+  components: {
+    TextbookList
+  },
+  data() {
+    return {
+        textbooks: [
+            {
+                id: 1,
+                name: 'Textbook 1',
+                author: 'Author 1',
+            },
+            {
+                id: 2,
+                name: 'Textbook 2',
+                author: 'Author 2',
+            }
+        ]
+    }
+  }
+}
 </script>
 
 <style>
---
+++ src/components/TextbookList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-for="textbook in textbooks">
+        <h1>{{ textbook.id }} {{ textbook.name }} {{ textbook.author }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+      textbooks: {
+        type: Array,
+        required: true
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
